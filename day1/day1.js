b6cfa01183e0cbeab87644537398d2e71525c3e0
rrbotlab <4213391+rrbotlab@users.noreply.github.com>
2025-02-25 09:44:58 -0300

    commit Day - 1

---
+++ day1/day1.js
@@ -0,0 +1,44 @@
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+document.getElementById('vars').innerHTML = " \
+  let numeroUm = 1;<br>\
+let stringUm = '1';<br>\
+let numeroTrinta = 30;<br>\
+let stringTrinta = '30';<br>\
+let numeroDez = 10;<br>\
+let stringDez = '10';<br>\
+"
+
+
+/*if (COMPARAR O numeroUm e a stringUm) {*/
+if(numeroUm == stringUm && numeroUm !== stringUm ){
+  document.getElementById('res1').innerHTML = 'As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes';
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+} else {
+  document.getElementById('res1').innerHTML = 'As variáveis numeroUm e stringUm não tem o mesmo valor';
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+}
+
+/*if (COMPARAR O numeroTrinta e a stringTrinta) {*/
+if(numeroTrinta == stringTrinta && numeroTrinta === stringTrinta){
+  document.getElementById('res2').innerHTML = 'As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo';
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo');
+} else {
+  document.getElementById('res2').innerHTML = 'As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo';
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
+}
+
+/*if (COMPARAR O numeroDez e a stringDez) {*/
+if(numeroDez == stringDez && numeroDez !== stringDez){
+  document.getElementById('res3').innerHTML = 'As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes';
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+} else {
+  document.getElementById('res3').innerHTML = 'As variáveis numeroDez e stringDez não tem o mesmo valor';
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+}
+
