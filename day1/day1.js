36bd7cff4ef0376cf49cad574a5cbef68d4b69ab
rrbotlab <4213391+rrbotlab@users.noreply.github.com>
2025-02-25 14:39:54 -0300

    commentario e remove codigo redundante

--- day1/day1.js
+++ day1/day1.js
@@ -7,13 +7,23 @@ let stringDez = '10';
 
 document.getElementById('vars').innerHTML = " \
   let numeroUm = 1;<br>\
-let stringUm = '1';<br>\
-let numeroTrinta = 30;<br>\
-let stringTrinta = '30';<br>\
-let numeroDez = 10;<br>\
-let stringDez = '10';<br>\
+  let stringUm = '1';<br>\
+  let numeroTrinta = 30;<br>\
+  let stringTrinta = '30';<br>\
+  let numeroDez = 10;<br>\
+  let stringDez = '10';<br>\
 "
 
+// Há um bug caso numeroUm e stringUm sejam do mesmo tipo e mesmo valor
+// Neste caso será executado o else porém com msg incorreta (valores são iguais)
+// Para corrigir a partir do else:
+// }else if(numeroUm != stringUm){
+//   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+// else{
+//   console.log('As variáveis numeroUm e stringUm são identicas');
+// }
+//
+// O mesmo para numeroDez stringDez
 
 /*if (COMPARAR O numeroUm e a stringUm) {*/
 if(numeroUm == stringUm && numeroUm !== stringUm ){
@@ -24,8 +34,9 @@ if(numeroUm == stringUm && numeroUm !== stringUm ){
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
 }
 
+// removida comparação redundante
 /*if (COMPARAR O numeroTrinta e a stringTrinta) {*/
-if(numeroTrinta == stringTrinta && numeroTrinta === stringTrinta){
+if(numeroTrinta === stringTrinta){
   document.getElementById('res2').innerHTML = 'As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo';
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo');
 } else {
